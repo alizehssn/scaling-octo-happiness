ab7c74d279482893e19c5eb86643061f4aace7d4
Vincent <vrdphone@gmail.com>
2020-08-31 20:50:32 -0500

    added tabs to html, got random recipe search to work

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -9,16 +9,16 @@
 $("#submitButton").on("click", function(event) {
     event.preventDefault();
 
-    let itemToSearch = $("#searchBar").val();
+    let itemToSearch2 = $("#searchBar").val();
 
 
-    searchItem(itemToSearch);
+    searchItem(itemToSearch2);
 })
 
-function searchItem(itemToSearch) {
-    console.log(itemToSearch);
+function searchItem(itemToSearch2) {
+    console.log(itemToSearch2);
 
-    let queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + itemToSearch + "&apiKey=0ba7a0fdd45c497a8afd81dae904a16c";
+    let queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + itemToSearch2 + "&apiKey=0ba7a0fdd45c497a8afd81dae904a16c";
 
 
 
@@ -28,6 +28,58 @@ function searchItem(itemToSearch) {
     }).then(function(response) {
         console.log(queryURL);
         console.log(response);
+
+
+
+    })
+}
+//
+//THIRD SEARCH BAR WHICH IS RANDOMLY
+//#3
+
+
+$("#submitButton3").on("click", function(event) {
+    event.preventDefault();
+
+
+
+
+    searchItem();
+})
+
+function searchItem() {
+
+
+    let queryURL3 = "https://api.spoonacular.com/recipes/random?&number=5&apiKey=0ba7a0fdd45c497a8afd81dae904a16c";
+
+
+
+    $.ajax({
+        url: queryURL3,
+        method: "GET"
+    }).then(function(response3) {
+        console.log(queryURL3);
+        console.log(response3);
+
+        for (var x = 0; x < response3.recipes.length; x++) {
+            let recipe = response3.recipes[x];
+            let mainDiv3 = $("<div>").addClass("mainDiv3");
+            let title3 = $("<h3>").text(recipe.title);
+            let imageEl3 = $("<img>").attr("src", recipe.image).attr("style", "width: 300px");
+            let about3 = $("<div>").html(recipe.summary);
+
+
+            mainDiv3.append(title3, imageEl3, about3);
+
+
+            $("#attachHere3").append(mainDiv3);
+
+
+
+        }
+
+
+
 
 
 
