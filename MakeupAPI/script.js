eddd820730940cee805f741506cc2cfaa486ef95
Botticelli <alizehssn@gmail.com>
2020-09-02 21:40:07 -0500

    Fix dir casing

--- MakeupAPI/script.js
+++ MakeupAPI/script.js
@@ -1,39 +1,92 @@
-//https://makeup-api.herokuapp.com/
+// //https://makeup-api.herokuapp.com/
 
 
-///User Selects Which Search Criteria/Tag they are looking for
-//Can search using the brand or product(all mascara with tag or all brand with tag or both
-//What to Do:
-//Create on click function for button
-//When Button click if the producttype search request return products that match that parameter,
-//when both are clicked return brandnames that contain products that have the product tag
-//Both functionality in same button
+//Functionality:
+//When user selects check boxes those values if checked are stored as productTag
+//User can search by products,brands, or both
+//the results are displayed to the user
 
-//Global Variables
 
-const baseUrl = "http://makeup-api.herokuapp.com/api/v1/products.json?"
 
-//Function to be Called in On-Click
-function pullMakeupProducts() {
-    //get input from search bars and append brand=&prdocucttype
-    if ("#searchProduct" != null) {
-        var searchByProductAnd = $("#searchProduct").val().trim()
+
+
+
+$("#searchBy").on("click", function(event) {
+    event.preventDefault();
+    searchMakeup();
+})
+
+
+
+
+
+
+
+
+function searchMakeup() {
+    //Setting BaseUrl for API & Blank Arrays for Parameters
+    let baseUrl = "http://makeup-api.herokuapp.com/api/v1/products.json?"
+    let productTag = [];
+    let productBrand = [];
+    let productType = [];
+    //Setting the Product Tags from CheckBox HTML
+    $("#makeupFilters").children("input").each(function() {
+        if ($(this).prop("checked") === true) {
+            productTag.push($(this).val());
+            console.log(productTag)
+        }
+    })
+
+    baseUrl += "product_tags=" + productTag.join(",");
+    console.log(baseUrl)
+        //Setting Search Parameter for Product Type
+    $("#productType").children("option").each(function() {
+        if ($(this).prop("selected") === true) {
+            productType.push($(this).val());
+            console.log(productType)
+        }
+    })
+    baseUrl += "&product_type=" + productType
+    console.log(baseUrl)
+
+    //Setting Search Brand Type
+    productBrand = $("#productBrand").val();
+    console.log(productBrand)
+    if (productBrand != 0) {
+        baseUrl += "&brand=" + productBrand
+        console.log(baseUrl)
     }
-    if
-    else("#searchProduct" = null) {
-        var searchByProduct = $("#searchProduct").val().trim()
-    }
-    if
 
+    // let resultsLength = $("#selectResultsQ").val();
+
+    //AJAX Call
+
+    $.ajax({
+        url: baseUrl,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        $("#resultsContainer").empty();
+        if (response.length === 0) {
+            let divEl = $("<div>").text("No Results :(");
+            $("#resultsContainer").append(divEl)
+        }
+        for (var i = 0; i < response.length; i++) {
+            let brand = $("<h1>").text(response[i].brand);
+            let img = $("<img>").attr("src", response[i].image_link).attr("style", "width: 300px");
+            let prod = $("<h2>").text(response[i].product_type);
+            let cat = $("<p>").text(response[i].category);
+            let link = $("<p>").text(response[i].product_link);
+            let matchedTags = $("<p>").text(response[i].tag_list);
+
+
+            $("#resultsContainer").append(brand, img, prod, cat, link, matchedTags)
+        }
+
+
+
+    })
 
 
 
 }
-
-
-
-//SearchBtn On clik:
-
-$("searchBtn").on("click", function() {
-
-})
